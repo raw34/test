6dbfcaba937761505c67788c4608bdba719ebda6
raw34 <raw34@sina.com>
2014-03-03 00:21:39 +0800

    more test

--- php/function.php
+++ php/function.php
@@ -32,4 +32,29 @@ function josehpus2($n, $m) {
 echo josehpus1(10, 7);
 echo '<br/>';
 echo josehpus2(10, 7);
+
+//冒泡排序法
+function bubble_sort($a) {
+    $len = count($a);
+    if ($len <= 1) {
+        return $a;
+    }
+    $temp = null;
+    for ($i = 0; $i < $len; $i++) {
+        for ($j = $len - 1; $j > $i; $j--) {
+            if ($a[$j] < $a[$j - 1]) {
+                $temp = $a[$j - 1];
+                $a[$j - 1] = $a[$j];
+                $a[$j] = $temp;
+            }
+        }
+    }
+    return $a;
+}
+
+$a = range(1, 10);
+shuffle($a);
+echo '<pre>'; var_dump($a); echo '</pre>';
+$a = bubble_sort($a);
+echo '<pre>'; var_dump($a); echo '</pre>';
 ?>
---
+++ php/test6.php
@@ -0,0 +1,29 @@
+<?php
+require_once '../../Pinyin/PyDict.php';
+require_once '../../Pinyin/Pinyin.php';
+
+$file = fopen('tags1.csv', 'r');
+$tags = array();
+while (! feof($file)) {
+    $row = fgets($file);
+    $arr = explode(',', trim($row));
+    $i = 0;
+    foreach ($arr as $v) {
+        //echo '<br/>', $i, ' = ', $v;
+        if (trim($v) != '') {
+            $pinyin = Pinyin::getPinyin($v);
+            $tags[$i][] = array($v, $pinyin[0]);
+        }
+        $i++;
+    }
+}
+$json = json_encode($tags);
+echo $json;
+//echo '<pre>'; var_dump($tags); echo '</pre>';
+/*$i = 1;
+$j = count($tags) + 1;
+foreach ($tags as $v) {
+    
+}
+ */
+?>
