7586ca4db20a7f1cf5ab8e8d7c5d70de3182b407
raw34 <raw34@sina.com>
2014-05-20 23:59:22 +0800

    update

--- php/class.php
+++ php/class.php
@@ -4,6 +4,8 @@
  */
 class Test
 {
+    static protected $car = 20;
+    //public $car;
     private $item;
     /**
      * 
@@ -11,8 +13,25 @@ class Test
     public function __construct()
     {
         $this->item = 10;
+        //$this->car = 20;
     }
 }
-$obj = new Test();
-echo '<pre>'; var_dump($obj); echo '</pre>';
-?>
+//$obj = new Test();
+//echo '<pre>'; var_dump($obj); echo '</pre>';
+/**
+ * test1
+ */
+class Test1 extends Test
+{
+    /**
+     * 
+     */
+    public function __construct()
+    {
+        //$this->car = 30;
+        self::$car = 30;
+        echo 'this->car = ', self::$car, ' parent->car = ', parent::$car;
+    }
+}
+
+new Test1();
