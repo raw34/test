5388ea9a48915514e76971f1a06a81f0f5bd7213
raw34 <raw34@sina.com>
2014-06-22 09:45:52 +0800

    update

---
+++ perl/file.pl
@@ -0,0 +1,28 @@
+#!/usr/local/bin/perl
+use encoding "utf-8";
+use Data::Dumper;
+
+open(INPUT, "<:utf8", "eee2.md") or die("open error:$!");
+my @lines = <INPUT>;
+close(INPUT);
+#print scalar(@lines);
+
+my $str_input = join("", @lines);
+my @arr_input = split(/#{10}/, $str_input);
+#print scalar(@arr_input);
+my %hash_output = ();
+foreach (@arr_input) {
+    my ($title, $content) = split(/#{3}/, $_);
+    $title_s = substr($title, 0, 10);
+    $hash_output{$title_s} = {'title' => $title, 'content' => $content};
+}
+
+#print scalar(keys(%hash_output));
+my $str_output = '';
+foreach my $key(keys %hash_output) {
+    $str_output .= "$hash_output{$key}{'title'}\n$hash_output{$key}{'content'}";
+}
+
+open(OUTPUT, ">:utf8", "eee3.md") or die("open error:$!");
+print OUTPUT ($str_output);
+close(OUTPUT);
