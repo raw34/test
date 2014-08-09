58cb60dc57e2ef54690850fa2c2189e2e8282a38
raw34 <raw34@sina.com>
2014-08-10 00:46:46 +0800

    update

--- perl/file2.pl
+++ perl/file2.pl
@@ -1,8 +1,9 @@
 #!/usr/local/bin/perl
 use encoding "utf-8";
+use Encode;
 use Data::Dumper;
 
-open(INPUT, "<:utf8", "lamp7.md") or die("open error:$!");
+open(INPUT, "<:utf8", "lamp8.md") or die("open error:$!");
 my @lines = <INPUT>;
 close(INPUT);
 #print scalar(@lines);
@@ -10,35 +11,43 @@ close(INPUT);
 my $str_input = join("", @lines);
 my @arr_input = split('---', $str_input);
 my %hash_output = ();
+my @arr_output = ();
+my $str_output = '';
 my $i = 0;
 foreach (@arr_input) {
-    $_ =~ /#(\d{1,3})、(.*?([A-Z])$)/;
+    #$_ =~ /#(\d{1,3})、(.*?([A-Z]$))/;
+    $_ =~ /\n#(\d{1,3})、.*?([A-Z])[\r\n]/;
     my $order = $1;
-    my $title = $2;
-    my $type = $3;
-    $hash_output{$type}{$order} = $_;
+    my $type = $2;
+    $_ =~ s/#$order/#$i/;
+=pod
+    if ($i >= 200) {
+        print "\norder = ", $order, ' type = ', $type;
+    }
+    my $key = $_ =~ /undone/i ? $i + 1000000 : $i;
+    print "\n", $key;
+    $arr_output[$key] = $_;
+=cut
+    if ($type ne '') {
+        $hash_output{$type}[$i] = $_;
+    }
+    $i++;
 }
-
 
 =pod
-my @arr_input = split(/#{10}/, $str_input);
-#print scalar(@arr_input);
-my %hash_output = ();
-foreach (@arr_input) {
-    my ($title, $content) = split(/#{3}/, $_);
-    $title =~ /(\d+)、/;
-    $title_s = $1;
-    $hash_output{$title_s} = {'title' => $title, 'content' => $content};
-}
+    #undone延后
+    $str_output = join("---\n", @arr_output);
+    open(OUTPUT, ">:utf8", "lamp8.md") or die("open error:$!");
+    print OUTPUT ($str_output);
+    close(OUTPUT);
 =cut
 
-#print scalar(keys(%hash_output));
-my $str_output = '';
-foreach my $key(sort {$a <=> $b} keys %hash_output) {
-    print $key, "\n";
-    $str_output .= "$hash_output{$key}{'title'}\n$hash_output{$key}{'content'}";
-}
+#print Dumper(%hash_output);
 
-open(OUTPUT, ">:utf8", "eee3.md") or die("open error:$!");
-print OUTPUT ($str_output);
-close(OUTPUT);
+foreach my $type(keys(%hash_output)) {
+    print "\ntype = ", $type;
+    $str_output = join('', @{$hash_output{$type}});;
+    open(OUTPUT, ">:utf8", "$type.md") or die("open error:$!");
+    print OUTPUT ($str_output);
+    close(OUTPUT);
+}
