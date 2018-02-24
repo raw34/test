36829dfbcc7fcac053acc3d08da9d4e6263c6f87
raw34 <raw34@sina.com>
2018-02-24 17:09:54 +0800

    update

--- sh/remote.sh
+++ sh/remote.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+dirname=`pwd`
+
 remotecmd="export PATH=$PATH"
 
 remotecmd="$remotecmd \
-    && find . -maxdepth 1 -ctime +30 | xargs rm -rf "
+    && find . -name '"$dirname"_*' -maxdepth 1 -ctime +30 | xargs rm -rf "
 
 echo $remotecmd
 
