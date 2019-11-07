b377ff0fe4135a8f573a9061332ae4df620d6875
raw34 <raw34@sina.com>
2019-11-07 18:50:52 +0800

    update

--- php/openssl.php
+++ php/openssl.php
@@ -19,17 +19,21 @@ $public_key = file_get_contents(__DIR__ . '/../rsa_public_key.pem');
 $private_key_res = openssl_get_privatekey($private_key);
 $public_key_res = openssl_get_publickey($public_key);
 
+$alg = OPENSSL_ALGO_SHA256;
+
 //create signature
 // openssl_sign($data, $signature, $private_key_res, "sha1WithRSAEncryption");
-openssl_sign($data, $signature, $private_key_res);
+openssl_sign($data, $signature, $private_key_res, $alg);
 
 openssl_free_key($private_key_res);
 
 //verify signature
 // $ok = openssl_verify($data, $signature, $public_key_res, OPENSSL_ALGO_SHA1);
-$ok = openssl_verify($data, $signature, $public_key_res);
+$ok = openssl_verify($data, $signature, $public_key_res, $alg);
 
 openssl_free_key($public_key_res);
+
+echo 'signature = ' . bin2hex($signature) . "\n";
 
 if ($ok == 1) {
     echo "valid\n";
