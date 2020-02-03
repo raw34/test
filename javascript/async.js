00b631c66b9f0a415b5f22383285d969d0e59fe3
raw34 <raw34@sina.com>
2020-02-03 15:32:49 +0800

    update

--- javascript/async.js
+++ javascript/async.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
+var readFile = function (fileName){
+  return new Promise(function (resolve, reject){
+    fs.readFile(fileName, function(error, data){
+      if (error) reject(error);
+      resolve(data);
+    });
+  });
+};
+
 async function test() {
   var name = path.resolve() + '/javascript/helloworld.js';
-  var file = await fs.promises.readFile(name);
+  // var file = await fs.promises.readFile(name);
+  var file = readFile(name);
 
   return file;
 }
