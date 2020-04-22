4aa20f576a6da8779fda61dab44f0ec75583eb09
raw34 <raw34@sina.com>
2020-04-22 18:43:31 +0800

    update

--- shell/array.sh
+++ shell/array.sh
@@ -9,15 +9,19 @@ arr1=(1 2 3 4)
 echo ${arr[0]}
 
 declare -A phone
-phone=([jim]="135" [tom]="136" [lucy]="158" )
+phone=(
+[jim]="135"
+[tom]="136"
+[lucy]="158"
+)
 
 echo ${phone[jim]}
 
 
-# for key in ${!phone[*]}
-# do
-    # echo "$key -> ${phone[$key]}"
-# done
+for key in ${!phone[*]}
+do
+   echo "$key -> ${phone[$key]}"
+done
 
 declare -A dict=([a]="123" [b]="1234" )
 
