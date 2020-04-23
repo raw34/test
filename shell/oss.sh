3d1988ea7aea56e9d93eeed3093e6929d3d6a9d7
raw34 <raw34@sina.com>
2020-04-23 11:40:43 +0800

    update

--- shell/oss.sh
+++ shell/oss.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 # 接收参数
+bucket=videos-buket
+dest=$(date +%Y%m%d)
 src=$(date +%Y%m%d)
-bucket=test
-type=video
 ext=mp4
+type=video
+
+while getopts ":b:d:e:s:t:" optname
+do
+  case "$optname" in
+    b) bucket=$OPTARG ;;
+    d) dest=$OPTARG ;;
+    e) ext=$OPTARG ;;
+    s) src=$OPTARG ;;
+    t) type=$OPTARG ;;
+    *) echo "usage: $0 [-b] [-d] [-e] [-t]" ;;
+  esac
+done
 
 # 遍历文件
 for file in $(ls "$src" | grep "\.$ext") ; do
   # 计算文件名
   name="$(echo -n "${file//.$ext/}" | md5sum | cut -d " " -f1)"
 
-  # 修改文件名
-  mv "$src"/"$file" "$src"/v2_"$name"_"$type"_"$ext"
+  # 上传文件
+  echo "dest name => ""$dest"/v2_"$name"_"$type"_"$ext"
+  ossutil cp -u -f "$src"/"$file" oss://"$bucket"/"$dest"/v2_"$name"_"$type"_"$ext"
 done
-
-# 上传文件
-#ossutil cp "$src" oss://"$bucket"/"$src" --include "_$ext" -r
