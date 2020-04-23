5d1690c482f4070660373f797c881c47203ad342
raw34 <raw34@sina.com>
2020-04-23 14:57:49 +0800

    update

--- shell/oss.sh
+++ shell/oss.sh
@@ -23,7 +23,10 @@ for file in $(ls "$src" | grep "\.$ext") ; do
   # 计算文件名
   name="$(echo -n "${file//.$ext/}" | md5sum | cut -d " " -f1)"
 
+  # 计算文件大小
+  size="$(wc -c "$src/$file" | sed "s/^ *//" | sed "s/\([0-9]*\) .*/\1/")"
+
   # 上传文件
-  echo "dest name => ""$dest"/v2_"$name"_"$type"_"$ext"
-  ossutil cp -u -f "$src"/"$file" oss://"$bucket"/"$dest"/v2_"$name"_"$type"_"$ext"
+  echo "src_name = $file, size = $size, """dest_name = "$dest"/v2_"$name"_"$type"_"$ext" >> "$src"/result.log
+#  ossutil cp -u -f "$src/$file" oss://"$bucket"/"$dest"/v2_"$name"_"$type"_"$ext"
 done
