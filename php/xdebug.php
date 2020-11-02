d5f2c8a8c4c5234770d601688568eec7b0db8ce0
raw34 <raw34@sina.com>
2020-11-03 06:54:06 +0800

    update

--- php/xdebug.php
+++ php/xdebug.php
@@ -1,6 +1,23 @@
 <?php
-$arr = [];
-echo $arr['a'];
+//$arr = [];
+//echo $arr['a'];
 
-$trace = array_slice(array_reverse(xdebug_get_function_stack()), 3, -1);
+//$trace = array_slice(array_reverse(xdebug_get_function_stack()), 3, -1);
+
+class xdebug {
+    function dumpStack($a): void
+    {
+//        $trace = xdebug_get_function_stack();
+        $trace = array_slice(array_reverse(xdebug_get_function_stack()), 3, -1);
+        var_dump($trace);
+    }
+
+    function fixArray($b): void {
+        echo $b['last'];
+        $this->dumpStack($b);
+    }
+}
+
+$s = new xdebug();
+$ret = $s->fixArray(array('Derick'));
 
