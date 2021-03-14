42055879fb541272f42092fb09f806bcdc2d4c7d
raw34 <raw34@sina.com>
2021-03-14 21:55:24 +0800

    update

--- go/http_test.go
+++ go/http_test.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
-	"raw34.xyz/test/go/net/http"
+	"net/http"
+	"raw34.xyz/test/go/io"
+	netHttp "raw34.xyz/test/go/net/http"
 	"testing"
 )
 
 func Test_httpGet(t *testing.T) {
 	url := "https://petstore.swagger.io/v2/user/raw34"
-	client := &HttpClient{}
-	client.SetClient(&http.ClientMock{})
 
-	res := client.Get(url)
+	ctrl := gomock.NewController(t)
+	ctrl.Finish()
+	requestStub, _ := http.NewRequest("GET", url, nil)
+	ioReadCloserMock := io.NewMockReadCloser(ctrl)
+	ioReadCloserMock.EXPECT().Read(nil).Return(nil)
+	ioReadCloserMock.EXPECT().Close().Return()
+	responseStub := &http.Response{}
+	responseStub.Body = ioReadCloserMock
+	clientMock := netHttp.NewMockClientInterface(ctrl)
+	clientMock.EXPECT().Do(requestStub).Return(responseStub, nil)
+
+	httpClient := HttpClient{}
+	httpClient.SetClient(clientMock)
+
+	res := httpClient.Get(url)
 
 	assert.Equal(t, nil, res)
-	//stub := `{"id":100,"username":"raw34","firstName":"Randy","lastName":"Chang","email":"raw0034@gmail.com","password":"xxxxxx","phone":"100000","userStatus":0}`
-	//log.Println(stub)
+	//stub := `{"id":100,"username":"raw34","firstName":"Randy","lastName":"Chang","email":"raw0034@gmail.com","password":"xxxxxx","phone":"100000","userStatus":0}` log.Println(stub)
 }
 
 func Test_httpPost(t *testing.T) {
