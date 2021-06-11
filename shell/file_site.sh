c66b27b7ee659b73e81000464f7458bf372d368e
raw34 <raw0034@gmail.com>
2021-06-11 11:12:40 +0800

    update

--- shell/file_site.sh
+++ shell/file_site.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 
-#log_dir="/alidata/log/www/dsp"
 log_dir="/tmp/log/www/dsp"
-#file_dir="/alidata/www/static_img/Upload/baidu_report"
 file_dir="${HOME}/Downloads"
 file_name="${log_dir}/big_files_$(date +%Y%m%d%H).log"
 
