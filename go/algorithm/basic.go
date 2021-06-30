964c070fefc7dcdae5b3ce666a2c1360d17a300c
raw34 <raw34@sina.com>
2021-06-30 23:22:51 +0800

    update

---
+++ go/algorithm/basic.go
@@ -0,0 +1,95 @@
+package algorithm
+
+//maximum depth of binary tree
+//balanced binary tree
+//binary tree maximum path sum
+//lowest common ancestor of a binary tree
+//binary tree level order traversal
+//binary tree level order traversal ii
+//binary tree zigzag level order traversal
+//validate binary search tree
+//insert into a binary search tree
+//
+//remove duplicates from sorted list
+//remove duplicates from sorted list ii
+//reverse linked list
+//reverse linked list ii
+//merge two sorted lists
+//partition list
+//sort list
+//reorder list
+//linked list cycle
+//linked list cycle ii
+//palindrome linked list
+//copy list with random pointer
+//
+//min stack
+//evaluate reverse polish notation
+//decode string
+//binary tree inorder traversal
+//clone graph
+//number of islands
+//largest rectangle in histogram
+//implement queue using stacks
+//01 matrix
+//
+//single number
+//single number ii
+//single number iii
+//number of 1 bits
+//counting bits
+//reverse bits
+//
+//search for range
+//search insert position
+//search a 2d matrix
+//first bad version
+//find minimum in rotated sorted array
+//find minimum in rotated sorted array ii
+//search in rotated sorted array
+//search in rotated sorted array ii
+//
+//triangle
+//minimum path sum
+//unique paths
+//unique paths ii
+//
+//climbing stairs
+//jump game
+//jump game ii
+//palindrome partitioning ii
+//longest increasing subsequence
+//word break
+//
+//longest common subsequence
+//edit distance
+//
+//coin change
+//backpack
+//backpack ii
+//
+//reverse string
+//swap nodes in pairs
+//unique binary search trees ii
+//fibonacci number
+//
+//minimum window substring
+//permutation in string
+//find all anagrams in a string
+//longest substring without repeating characters
+//
+//validate binary search tree
+//insert into a binary search tree
+//delete node in a bst
+//balanced binary tree
+//
+//subsets
+//subsets ii
+//permutations
+//permutations ii
+//
+//combination sum
+//letter combinations of a phone number
+//palindrome partitioning
+//restore ip addresses
+//permutations
